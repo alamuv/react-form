02990cb0409132542b7822e10f861b3970b34684
beardedtim <timiroberts@gmail.com>
2017-09-12 09:35:12 -0700

    build(App): Fix build script

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,21 @@
 import babel from 'rollup-plugin-babel'
 import uglify from 'rollup-plugin-uglify'
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
 
 var config = {
   format: 'umd',
-  moduleName: 'ReactMenu',
+  moduleName: 'ReactForm',
   plugins: [
     babel({
       exclude: 'node_modules/**',
       plugins: ['external-helpers']
+    }),
+    resolve(),
+    commonjs({
+      namedExports: {
+        'node_modules/ramda/index.js': ['ramda']
+      }
     })
   ],
   external: ['react', 'prop-types', 'classnames'],
